886d2ac93a16dda7f11bdc9a97c94cc6fafaba3d
rivkabn <rivkabn81@gmail.com>
2022-10-06 01:49:59 +0300

    second commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
         let token = req.header("Authorization");
         if (!token) return res.status(401).send("Access denied. no token provided");
 
-        // check the token
+        // check the token  
         let payload = jwt.verify(token, process.env.jwtKey);
         // save the payload
         req.payload = payload;
